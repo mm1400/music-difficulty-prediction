946bf045492802e56b1904ccd2a00f92edb91f9c
mm1400 <marin.melentii@gmail.com>
2025-04-16 23:45:25 -0400

    Add GridSearchCV for XGB hyper parameters

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.pipeline import make_pipeline
 from sklearn.feature_selection import r_regression, SelectPercentile
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, GridSearchCV
 from xgboost import XGBRegressor
 import pandas as pd
 import pickle
@@ -42,14 +42,30 @@ rng = np.random.RandomState(0)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=rng)
 
+# params = {
+#         'n_estimators': [100, 200, 500, 1000],
+#         'gamma': [0.005, 0.01, 0.1, 0],
+#         'max_depth': [1, 2, 3, 6, 9],
+#         'learning_rate': [0.001, 0.01, 0.1, 0.015, 1],
+#         'min_child_weight': [1, 2, 3],
+#         }
+
+# grid_search = GridSearchCV(XGBRegressor(), params, scoring= 'neg_mean_squared_error')
+
+# grid_search.fit(X_train, y_train)
+
+# print("Best parameters:", grid_search.best_params_)
+# print("Best cross-validation score:", grid_search.best_score_)
+
+# df = pd.DataFrame(grid_search.cv_results_)
+# df.to_csv('grid_search_results.csv', index=False)
+
+# Best parameters for XGB: {'gamma': 0.01, 'learning_rate': 0.1, 'max_depth': 9, 'min_child_weight': 3, 'n_estimators': 500}
+
 models = {
     'Random Forest': RandomForestRegressor(n_estimators=100, random_state=rng),
     'SVR (RBF)': make_pipeline(SVR(kernel='rbf')),
-    # 'lasso': make_pipeline(RobustScaler(), Lasso(alpha=0.1)),
-    # 'Enet': make_pipeline(RobustScaler(), ElasticNet(alpha=0.5)),
-    # 'KRR': KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5),
-    # 'LinearRegression': LinearRegression(),
-    'XGB':  XGBRegressor(),
+    'XGB':  XGBRegressor(gamma=0.01, learning_rate=0.01, max_depth=9, min_child_weight=3, n_estimators=500, random_state=rng),
 }
 
 #Validation function
