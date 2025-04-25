83ce7c320da2af737a6a26b7962588ce19510c92
mm1400 <marin.melentii@gmail.com>
2025-04-24 21:14:06 -0400

    Update AverageModels imports

--- streamlit/AveragingModels.py
+++ streamlit/AveragingModels.py
@@ -1,18 +1,6 @@
 from sklearn import clone
 from sklearn.base import RegressorMixin, TransformerMixin, BaseEstimator
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.svm import SVR
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
-from sklearn.pipeline import make_pipeline
-from sklearn.feature_selection import r_regression, SelectPercentile
-from sklearn.model_selection import KFold, GridSearchCV
-from xgboost import XGBRegressor
-import pandas as pd
-import pickle
 import numpy as np
-import json
 
 class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
     def __init__(self, models, scaler):
